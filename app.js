0c65608b28589d9627244f58923eac58ac5a513f
Berdashev Roman <berdashev.r@konsom.ru>
2023-07-06 10:48:51 +0500

    added comments, check user and data

--- app.js
+++ app.js
@@ -84,43 +84,77 @@ let users = [
     }
 ];
 
-
 const app = express();
+// cors для всех источников
 app.use(cors());
+// парсер для json
 app.use(express.json());
-
+//метод для поиска индекса юзера по id
+const getUserIndexFromId = (userId) => {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === userId) {
+            return i;
+        }
+    }
+    return -1;
+};
+//методя для проверки данных при добавлении/изменении юзера
+//этот метод можно унифицировать путем передачи проверяемых полей в аргумент метода
+const checkBody = (body) => {
+    const fields = ["login", "email", "country", "sex", "age"];
+    return fields.every(field => field in body);
+};
+//запрос всех пользователей
 app.get("/users", (req, res) => {
     res.send(users);
 });
-
+//удаление пользователя по id
 app.delete("/user", (req, res) => {
     const userId = +req.query.userId;
-    users = users.filter(user => user.id !== userId);
+    //находим индекс юзера
+    const index = getUserIndexFromId(userId);
+    if (index === -1) {
+        return res.sendStatus(404);
+    }
+    users.splice(index, 1);
     res.send(users);
 });
-
+//добавление нового пользователя
 app.post("/user", (req, res) => {
-    if(!req.body) return res.sendStatus(400);
+    //проверяем тело запроса на его наличие и наличие в нем нужных нам полей
+    if (!req.body || !checkBody(req.body)) { 
+        return res.sendStatus(400);
+    }
     const user = req.body;
     user.id = users[users.length - 1].id + 1;
     users.push(user);
     res.send(users);
 });
-
+//получение данных пользователя по id
 app.get("/user", (req, res) => {
     const userId = +req.query.userId;
     const user = users.find(user => user.id === userId);
-    if (!user) return res.sendStatus(400);
+    if (!user) {
+        return res.sendStatus(404);
+    } 
     res.send(user);
 });
-
+//изменение данных пользователя по id
 app.put("/user", (req, res) => {
+    //проверяем тело запроса на его наличие и наличие в нем нужных нам полей
+    if (!req.body || !checkBody(req.body)) { 
+        return res.sendStatus(400);
+    }
     const userId = +req.query.userId;
+    //находим индекс юзера
+    const index = getUserIndexFromId(userId);
+    if (index === -1) {
+        return res.sendStatus(404);
+    }
     const editedUser = req.body;
     editedUser.id = userId;
-    users = users.map(user => user.id === userId ? editedUser : user);
+    users.splice(index, 1, editedUser);
     res.send(users);
 });
-
 
 app.listen(5000, () => console.log("сервер запущен"));
